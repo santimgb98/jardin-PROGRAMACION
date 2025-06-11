553eac10806fdf01bb42f64545146cbdfb5383b2
santimgb98 <santimgb98@email.com>
2025-06-11 17:14:46 +0200

    Práctica funcional

--- clases.js
+++ clases.js
@@ -1,56 +1,54 @@
 const prompt = require('prompt-sync')();
+const xlsx = require('xlsx');
 const luxon = require('luxon');
 const fs = require('fs');
+const { Script } = require('vm');
 let fecha_hora = luxon.DateTime;
 let hoy = fecha_hora.local();
 
 const plantasBDjson = JSON.parse(fs.readFileSync("./data.json", "utf8"));
 
-
 // Función asíncrona para regar la planta que lo necesite
-/**/    async function regarPlanta(nombre_planta) {
-/**/        return new Promise(() => {
-/**/            for(const planta of plantasBDjson){
-/**/                if (planta.nombre == nombre_planta){
-/**/                    planta.ultima_fecha_riego = hoy.toISODate();
-/**/                    let resultado =(`${planta.nombre} ha sido regado exitosamente!`);
-/**/                    return resultado;
-/**/                }
-/**/            }
-/**/        });
-/**/    }
+    async function regarPlanta(nombre_planta) {
 
-// Función asíncrona para guardar cada registro
-/**/    async function guardarRegistro(){}
-/**/
-/**/
-/**/
+        return new Promise((resolve) => {
+
+            for(let i=0; i<plantasBDjson.length; i++){
+                // Planta de la lista = planta del parámetro
+                if (plantasBDjson[i].nombre == nombre_planta){
+
+                    // Última fecha de riego se iguala a hoy
+                    plantasBDjson[i].ultima_fecha_riego = hoy.toISODate();
+                }
+            }
+            // Reescribe el contenido del JSON
+            resolve(fs.writeFileSync("./data.json",(JSON.stringify(plantasBDjson, null, 2)), 'utf8'));
+
+        })
+    }
 
 // Función asíncrona para revisar si hace falta regar la
 // planta especificada mediante parámetro
-/**/    async function revisarSiNecesitaRiego(planta_){
-/**/
-/**/       // fecha_hoy = fecha escrita (fecha de hoy)
-/**/       const fecha_hoy = fecha_hora.fromISO(hoy.toISODate());
-/**/       const ultima_fecha_riego = fecha_hora.fromISO(planta_.ultima_fecha_riego);
-/**/
-/**/       // diferencia = diferencia entre fechas en formato "días"
-/**/       const diferencia = fecha_hoy.diff(ultima_fecha_riego, 'days');
-/**/
-/**/       if(diferencia > planta_.frecuencia_riego)return true;
-/**/       if(diferencia <= planta_.frecuencia_riego)return false;
-/**/                
-/**/    }
+    function revisarSiNecesitaRiego(planta_){
 
-// Función asíncrona para crear log de riego por fecha
-/**/    async function crearLog_de_RiegoPorFecha(){}
-/**/
-/**/
-/**/
+        // fecha_hoy = fecha escrita (fecha de hoy)
+        const fecha_hoy = fecha_hora.fromISO(hoy.toISODate());
+
+        // Pasa a formato fecha la última fecha de riego
+        const ultima_fecha_riego = fecha_hora.fromISO(planta_.ultima_fecha_riego);
+
+        // diferencia = diferencia entre fechas en formato "días"
+        const diferencia = fecha_hoy.diff(ultima_fecha_riego, 'days');
+
+        // diferencia.days para ponerlo en formato número
+        if(diferencia.days > planta_.frecuencia_riego)return true; //Necesita riego
+        if(diferencia.days <= planta_.frecuencia_riego)return false; //No necesita riego
+         
+    }
 
 
 class Jardin{
-     constructor(nombre, tipo, frecuencia_riego, ultima_fecha_riego){
+    constructor(nombre, tipo, frecuencia_riego, ultima_fecha_riego){
         this.nombre = nombre;
         this.tipo = tipo;
         this.frecuencia_riego = frecuencia_riego;
@@ -60,6 +58,11 @@ class Jardin{
     async addPlanta(){
         console.clear();
         this.nombre = prompt('Nombre de la nueva planta: ').toUpperCase();
+        // ^[A-Z]+$: Determina que sea un nombre de la A-Z
+        // .test(): Método que comprueba si el nombre cumple la condición
+        while(!/^[A-Z]+$/.test(this.nombre)){
+            this.nombre = prompt('Nombre VÁLIDO de la nueva planta: ').toUpperCase();
+        }
         console.log('TIPOS (Selecciona su orden):');
         console.log('1. Flor');
         console.log('2. Árbol');
@@ -80,6 +83,9 @@ class Jardin{
                 }break;
             }
         this.frecuencia_riego = Number(prompt(`Cada cuantos días se riega ${this.nombre}: `));
+        while(isNaN(this.frecuencia_riego)===true || this.frecuencia_riego < 1){
+            this.frecuencia_riego = Number(prompt(`Cada cuantos días (número) se riega ${this.nombre}: `));
+        }
         // Selecciona el día de su creación (HOY) como el primero y último que se ha regado
         this.ultima_fecha_riego = hoy.toISODate();
 
@@ -96,43 +102,107 @@ class Jardin{
     }
     async listarPlantas(){
         console.clear();
-        // Recorre el JSON seleccionando nombre y tipo de cada planta y devuelve un mensaje
         for(const planta of plantasBDjson){
-            console.log(`La planta ${planta.nombre} es de tipo ${planta.tipo}`);
+            // Si es flor utilizar el atrículo "una", sino utilizar "un"
+            if(planta.tipo == "FLOR"){
+                console.log(`La planta ${planta.nombre} es una ${planta.tipo}`);
+            } else {
+                console.log(`La planta ${planta.nombre} es un ${planta.tipo}`);
+            }
         }
     }
-    async calcularTiempoUltimoRiego(){
+    calcularTiempoUltimoRiego(){
         console.clear();
+        let arrayPlantas = [];
+
         for(const planta of plantasBDjson){
-            console.log(planta.nombre);
+            arrayPlantas.push(planta.nombre);
         }
+        console.log(arrayPlantas);
         let opcion = prompt('Selecciona alguna planta de la lista: ').toUpperCase();
 
         for(const planta of plantasBDjson){
-            // fecha_hoy = fecha escrita (fecha de hoy)
+            // fecha_hoy = fecha actual a través de funciones con luxon (librería js)
             const fecha_hoy = fecha_hora.fromISO(hoy.toISODate());
+
+            // Pasa a formato fecha la última fecha de riego
             const ultima_fecha_riego = fecha_hora.fromISO(planta.ultima_fecha_riego);
 
             // diferencia = diferencia entre fechas en formato "días"
             const diferencia = fecha_hoy.diff(ultima_fecha_riego, 'days');
 
-            // Si el nombre de planta == objeto elegido
+            // Si el nombre de planta == prompt elegido
             if(planta.nombre == opcion){
                 console.clear();
                 // si la diferencia > 0  \ si la diferencia <= 0
                 if(diferencia ==0)console.log(`${opcion} se ha regado hoy`)
                 if(diferencia !=0)console.log(`Han pasado ${diferencia.days} días desde el último riego de ${opcion}`)
+            }else{
+                console.clear();
+                // Si planta != prompt elegido devuelve lo siguiente
+                console.log('Nombre de planta no válido');
+                break; // Evita que el mensaje aparezca varias veces en terminal
             }
         }
     }
     async necesarioRiegoSiNo(){
         console.clear();
+        let contador = 0;
         for(const planta of plantasBDjson){
             let riego = revisarSiNecesitaRiego(planta);
-            if(riego == true)console.log(`${planta.nombre} necesita ser regada ${planta.frecuencia_riego}`);
-            if(riego == false)console.log(`${planta.nombre} NO necesita riego ${planta.frecuencia_riego}`);
+            // Devuelve True si necesita regarse y False si No necesita
+            if(riego === true){
+                contador++;
+                console.log(`${planta.nombre} --> necesita ser regada`);
+            }
         }
+        if(contador >0){
+            let opcion = prompt('¿Quieres regar las plantas que lo necesitan? (si/no): ');
+        
+            console.clear();
+
+            for(const planta of plantasBDjson){
+            
+                let riego = revisarSiNecesitaRiego(planta);
+
+                if(opcion=='si' && riego === true){
+
+                    // Se riegan ÚNICAMENTE las plantas que necesiten ser regadas
+                    regarPlanta(planta.nombre);
+                    console.log('Se ha regado con éxito todo lo necesario');
+            
+                }else{console.clear()}
+            }
+        } else{console.log('No hay plantas que regar')}
+        
     }
+
+    guardarDatos(){
+        
+        console.clear();
+        // Convertir JSON a xls
+        const worksheet = xlsx.utils.json_to_sheet(plantasBDjson);
+        
+        // Crear un nuevo libro
+        const workbook = xlsx.utils.book_new();
+        xlsx.utils.book_append_sheet(workbook, worksheet, "Plantas");
+
+        const archivoXLSX = "plantas/plantas.xlsx";
+
+        // existsSync: función de la librería fileSystem para controlar la existencia
+        // de un archivo concreto, como es el caso actual.
+        if (!fs.existsSync(archivoXLSX)) {
+            // Si plantas.xlsx no existe lo crea
+            xlsx.writeFile(workbook, archivoXLSX);
+            console.log("Archivo xlsx creado con éxito en la carpeta Plantas");
+        } else {
+            // Si plantas.xlsx existe, lo sobreescribe actualizado
+            xlsx.writeFile(workbook, archivoXLSX);
+            console.log("Archivo xlsx actualizado con éxito en la carpeta Plantas");
+        }
+
+    }
+    
     async salir(){
         console.clear();
         console.log('Saliendo...');
@@ -141,6 +211,8 @@ class Jardin{
         })
     }
 }
+
+
 class Planta extends Jardin{
     super(nombre, tipo, frecuencia_riego, ultima_fecha_riego){
         this.nombre = nombre;
@@ -149,8 +221,6 @@ class Planta extends Jardin{
         this.ultima_fecha_riego = ultima_fecha_riego;
     }
 }
-
-
 
 module.exports = Jardin;
 module.exports = Planta;
--- main.js
+++ main.js
@@ -1,35 +1,35 @@
 const Jardin = require('./clases.js');
 const nuevoJardin = new Jardin();
 const readline = require('node:readline');
-const { stdin: input, stdout: output } = require('node:process');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-//  Función con promesa para elegir opción del menú
-/**/   function opcionMenu() {
-/**/       return new Promise((resolve) => {
-/**/            rl.question('Selecciona opción del menú: ', (respuesta) => {
-/**/                resolve(respuesta);
-/**/            });
-/**/        });
-/**/    }
-/**/    async function eleccion() {
-/**/        const opcion = await opcionMenu();
-/**/        return opcion;
-/**/    }
+//  Funciones con promesa para elegir opción del menú
+   function opcionMenu() {
+       return new Promise((resolve) => {
+            rl.question('Selecciona opción del menú: ', (respuesta) => {
+                resolve(respuesta);
+            });
+        });
+    }
+    async function eleccion() {
+        const opcion = await opcionMenu();
+        return opcion;
+    }
 
 //  Función asíncrona para la entrada en el programa
-/**/    async function entrada(){
-/**/        console.clear();
-/**/        console.log('Entrando al programa...');
-/**/        setTimeout(()=>{
-/**/            console.clear();
-/**/            main();
-/**/        },1500)
-/**/    }
+    async function entrada(){
+        console.clear();
+        console.log('Entrando al programa...');
+        setTimeout(()=>{
+            console.clear();
+            main();
+        },1500)
+    }
 
+//  Función asíncrona del menú del programa
 async function main(){
 
     while(true){
@@ -38,9 +38,10 @@ async function main(){
         console.log('MENÚ GESTOR DEL JARDÍN');
         console.log('1. Añadir nueva planta')
         console.log('2. Listar plantas')
-        console.log('3. Calcular útlimo tiempo de riego')
-        console.log('4. Necesario regar')
-        console.log('5. Salir y guardar')
+        console.log('3. Calcular tiempo desde el último riego')
+        console.log('4. Comprobar si es necesario el riego')
+        console.log('5. Guardar datos en xls')
+        console.log('6. Salir')
         console.log('---------------------------------');
 
         let opcion = await eleccion();
@@ -59,6 +60,9 @@ async function main(){
                 nuevoJardin.necesarioRiegoSiNo();
             }break;
             case '5':{
+                nuevoJardin.guardarDatos();
+            }break;
+            case '6':{
                 nuevoJardin.salir();
             }break;
             default:{
